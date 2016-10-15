a685ae8206001a4692f1a17b1d3f678fd827d817
mishlaev-personal <mishlaev@gmail.com>
2016-10-15 20:01:49 +0300

    Finished all tasks

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,22 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/414067.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      var elements = [];
+      for (x = 0; x < response.courses.completed.length; x++) {
+        var arrayElement = response.courses.completed[x];
+        var element = $('<div>' + '</div>')
+        .addClass('course')
+        .append('<h3>' + arrayElement.title + '</h3>')
+        .append($('<img>', {src:arrayElement.badge, title:arrayElement.title}))
+        .append($('<a href=' + arrayElement.url + 'target=_blank class="btn btn-primary">See sourse</a>'));
+        elements.push(element);
+      };
+      $('#badges').append(elements);
+    }
+  });
 
 });
