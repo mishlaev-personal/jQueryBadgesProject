75519ad5f3321969ceb6fcd69463bd09eb755a0e
mishlaev-personal <mishlaev@gmail.com>
2016-10-15 23:10:03 +0300

    Added el properties

--- src/assets/main.js
+++ src/assets/main.js
@@ -5,16 +5,29 @@ $(function() {
     url: 'https://www.codeschool.com/users/414067.json',
     dataType: 'jsonp',
     success: function(response) {
+      console.log(response);
       var elements = [];
+
       for (x = 0; x < response.courses.completed.length; x++) {
         var arrayElement = response.courses.completed[x];
         var element = $('<div>' + '</div>')
         .addClass('course')
-        .append('<h3>' + arrayElement.title + '</h3>')
-        .append($('<img>', {src:arrayElement.badge, title:arrayElement.title}))
-        .append($('<a href="' + arrayElement.url + '" target=_blank class="btn btn-primary">See sourse</a>'));
+        .append($('<h3>', {
+          text: arrayElement.title
+        }))
+        .append($('<img>', {
+          src:arrayElement.badge,
+          title:arrayElement.title
+        }))
+        .append($('<a>', {
+          href: arrayElement.url,
+          target: '_blank',
+          class: 'btn btn-primary',
+          text: 'See sourse'
+        }));
         elements.push(element);
       };
+
       $('#badges').append(elements);
     }
   });
